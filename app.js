d07a1c853eb0f94a71fdde9e7887df85c5b7534b
Olamilekan <nunuolamilekan@gmail.com>
2023-11-25 19:08:12 +0100

    updated expandNextStep function

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const planCard = document.querySelector(".plan__card");
 const setup = document.querySelector(".setup");
 const setupItems = document.querySelectorAll(".setup__item");
 const checkboxs = document.querySelectorAll(".setup__item__header__checkbox");
+const progress = document.querySelector(
+  ".setup__header__left__progress__value"
+);
+const count = document.querySelector(
+  ".setup__header__left__progress__wrapper__count"
+);
 
 const dropdownActiveClass = "navbar__right__dropdown--active";
 const alertDropdownActiveClass = "navbar__right__alert__dropdown--active";
@@ -57,25 +63,47 @@ setupItems.forEach((setupItem, index) => {
   });
 
   checkbox.addEventListener("click", () => {
+    const isChecked = checkbox.classList.contains(checkboxActiveClass);
+
     checkbox.classList.toggle(checkboxActiveClass);
 
-    const checked = document.querySelectorAll("." + checkboxActiveClass);
-    const progress = document.querySelector(
-      ".setup__header__left__progress__value"
-    );
-    const count = document.querySelector(
-      ".setup__header__left__progress__wrapper__count"
-    );
+    checkbox.setAttribute("aria-checked", !isChecked);
 
-    count.innerHTML = checked.length;
-    progress.style.setProperty("width", `${(72 / 5) * checked.length}px`);
+    updateProgress();
 
-    setTimeout(() => {
-      if (index + 1 < 5)
-        setupItems
-          .item(index + 1)
-          .querySelector(".setup__item__header")
-          .click();
-    }, 0);
+    setTimeout(() => expandNextStep(index), 0);
   });
 });
+
+const updateProgress = () => {
+  const checked = document.querySelectorAll("." + checkboxActiveClass);
+  count.innerHTML = checked.length;
+  progress.style.setProperty("width", `${(72 / 5) * checked.length}px`);
+};
+
+const expandNextStep = (index) => {
+  const nextElm = setupItems.item(index + 1);
+  const isNextChecked = nextElm
+    ? nextElm
+        .querySelector(".setup__item__header__checkbox")
+        .getAttribute("aria-checked") === "true"
+    : false;
+
+  if (
+    nextElm &&
+    !nextElm.classList.contains("setup__item--active") &&
+    !isNextChecked
+  ) {
+    nextElm.querySelector(".setup__item__header").click();
+  } else {
+    const items = [...setupItems].filter(
+      (item) =>
+        !item
+          .querySelector(".setup__item__header__checkbox")
+          .classList.contains(checkboxActiveClass)
+    );
+
+    if (items.length > 0)
+      items[0].querySelector(".setup__item__header").click();
+  }
+};
