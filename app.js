48d4e933b23dbe4195b0c6ada3cf1c9a6d71f454
Olamilekan <nunuolamilekan@gmail.com>
2023-11-27 11:37:53 +0100

    FEAT: Added focus to next step

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Selecting DOM elements
 const dropdown = document.querySelector(".navbar__right__dropdown");
 const dropdownRows = document.querySelectorAll(".navbar__right__dropdown__row");
 const userWrapper = document.querySelector(".navbar__right__user__wrapper");
+const bellIcon = document.querySelector(".navbar__right__bell__icon");
 const alertDropdown = document.querySelector(".navbar__right__alert__dropdown");
 const planCard = document.querySelector(".plan__card");
 const setup = document.querySelector(".setup");
@@ -12,18 +14,21 @@ const progress = document.querySelector(
 const count = document.querySelector(
   ".setup__header__left__progress__wrapper__count"
 );
+const filterIcon = document.getElementById("filter-icon");
 
+// CSS classes
 const dropdownActiveClass = "navbar__right__dropdown--active";
 const alertDropdownActiveClass = "navbar__right__alert__dropdown--active";
 const planCardHiddenClass = "plan__card--hidden";
 const setupItemsActiveClass = "setup__items--active";
 const checkboxActiveClass = "setup__item__header__checkbox--active";
 
+// Toggle dropdown visibility
 const toggleDropdown = () => {
   alertDropdown.classList.remove(alertDropdownActiveClass);
   dropdown.classList.toggle(dropdownActiveClass);
 
-  const isExpanded = userWrapper.attributes["aria-expanded"].value === "true";
+  const isExpanded = userWrapper.getAttribute("aria-expanded") === "true";
 
   if (isExpanded) {
     userWrapper.ariaExpanded = "false";
@@ -33,35 +38,65 @@ const toggleDropdown = () => {
   }
 };
 
+// Toggle alert dropdown visibility
 const toggleAlertDropdown = () => {
   dropdown.classList.remove(dropdownActiveClass);
   alertDropdown.classList.toggle(alertDropdownActiveClass);
+
+  const isExpanded = bellIcon.getAttribute("aria-expanded") === "true";
+
+  if (isExpanded) {
+    bellIcon.ariaExpanded = "false";
+  } else {
+    bellIcon.ariaExpanded = "true";
+    filterIcon.focus();
+  }
 };
 
+// Close dropdowns on Escape key press
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    if (alertDropdown.classList.contains(alertDropdownActiveClass)) {
+      toggleAlertDropdown();
+      bellIcon.focus();
+    }
+    if (dropdown.classList.contains(dropdownActiveClass)) {
+      toggleDropdown();
+      userWrapper.focus();
+    }
+  }
+});
+
+// Toggle plan card visibility
 const togglePlanCard = () => {
   planCard.classList.toggle(planCardHiddenClass);
 };
 
+// Toggle setup visibility
 const toggleSetup = () => {
   setup.classList.toggle(setupItemsActiveClass);
 };
 
+// Close other setup items when one is clicked
 const handleCloseRest = (index) => {
   [...setupItems]
     .filter((_, i) => i !== index)
     .forEach((item) => item.classList.remove("setup__item--active"));
 };
 
+// Setup item event listeners
 setupItems.forEach((setupItem, index) => {
   const setupItemHeader = setupItem.querySelector(".setup__item__header");
   const checkbox = setupItem.querySelector(".setup__item__header__checkbox");
 
+  // Click event on setup item header
   setupItemHeader.addEventListener("click", (event) => {
     event.preventDefault();
     setupItem.classList.add("setup__item--active");
     handleCloseRest(index);
   });
 
+  // Click event on checkbox
   checkbox.addEventListener("click", () => {
     const isChecked = checkbox.classList.contains(checkboxActiveClass);
 
@@ -69,18 +104,21 @@ setupItems.forEach((setupItem, index) => {
 
     checkbox.setAttribute("aria-checked", !isChecked);
 
+    // Update progress after checkbox click
     updateProgress();
 
     setTimeout(() => expandNextStep(index), 0);
   });
 });
 
+// Update progress based on checked checkboxes
 const updateProgress = () => {
   const checked = document.querySelectorAll("." + checkboxActiveClass);
   count.innerHTML = checked.length;
   progress.style.setProperty("width", `${(72 / 5) * checked.length}px`);
 };
 
+// Expand next setup item if available
 const expandNextStep = (index) => {
   const nextElm = setupItems.item(index + 1);
   const isNextChecked = nextElm
@@ -94,7 +132,7 @@ const expandNextStep = (index) => {
     !nextElm.classList.contains("setup__item--active") &&
     !isNextChecked
   ) {
-    nextElm.querySelector(".setup__item__header").click();
+    activateNextStep(nextElm);
   } else {
     const items = [...setupItems].filter(
       (item) =>
@@ -103,7 +141,16 @@ const expandNextStep = (index) => {
           .classList.contains(checkboxActiveClass)
     );
 
-    if (items.length > 0)
-      items[0].querySelector(".setup__item__header").click();
+    if (items.length > 0) {
+      activateNextStep(items[0]);
+    }
   }
+};
+
+const activateNextStep = (elm) => {
+  const nextStep = elm.querySelector(".setup__item__header");
+
+  nextStep.click();
+
+  nextStep.querySelector(".setup__item__header__checkbox").focus();
 };
